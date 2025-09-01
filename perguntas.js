152e3663a1975f3e9255ec01c1b324385202f65e
drak-00 <teles.erick@escola.pr.gov.br>
2025-09-01 19:02:46 -0300

    uiou

--- perguntas.js
+++ perguntas.js
@@ -1,17 +1,17 @@
 criaCartao(
-    'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'Futebol',
+    'Qual é o objetivo do futebol?',
+    'Fazer mais gols que o time adversário.'
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'Futebol',
+    'Quantos jogadores cada time tem em campo?',
+    '11 jogadores.'
 )
 
 criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Futebol',
+    'O que o goleiro pode fazer que os outros não podem?',
+    'Pegar a bola com as mãos dentro da área.'
 )
